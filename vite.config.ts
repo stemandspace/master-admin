e2c2cab78b2544f362bf4f9dc005d715a75dee25
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2025-03-06 11:03:54 +0530

    chore: add Express.js to project dependencies and configure base Vite path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
